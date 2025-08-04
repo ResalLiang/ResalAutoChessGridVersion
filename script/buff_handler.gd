6e71ac25258812db411d1c057505cba8222f8c92
Resal <60034532+ResalLiang@users.noreply.github.com>
2025-08-04 15:36:02 +0800

    Create buff_handler.gd

---
+++ script/buff_handler.gd
@@ -0,0 +1,85 @@
+class_name Buff_handler
+
+var is_immunity := false
+var immunity_duration := 0
+
+var spd_modifier := 0
+var spd_modifier_duration := 0
+
+var attack_rng_modifier := 0
+var attack_rng_modifier_duration := 0
+
+var attack_spd_modifier := 0
+var attack_spd_modifier_duration := 0
+
+var attack_dmg_modifier := 0
+var attack_dmg_modifier_duration := 0
+
+var continuous_hp_modifier := 0
+var continuous_hp_modifier_duration := 0
+
+var continuous_mp_modifier := 0
+var continuous_mp_modifier_duration := 0
+
+var armor_modifier := 0
+var armor_modifier_duration := 0
+
+var max_hp_modifier := 0
+var max_hp_modifier_duration := 0
+
+var critical_rate_modifier := 0.0
+var critical_rate_modifier_duration := 0
+
+var evasion_rate_modifier := 0.0
+var evasion_rate_modifier_duration := 0
+
+
+func start_turn_update():
+
+	immunity_duration = max(0, immunity_duration - 1)
+	is_immunity = immunity_duration > 0
+
+	spd_modifier_duration = max(0, spd_modifier_duration - 1)
+	spd_modifier = 0 if spd_modifier_duration <= 0 else spd_modifier
+
+	attack_rng_modifier_duration = max(0, attack_rng_modifier_duration - 1)
+	attack_rng_modifier = 0 if attack_rng_modifier_duration <= 0 else attack_rng_modifier
+
+	attack_spd_modifier_duration = max(0, attack_spd_modifier_duration - 1)
+	attack_spd_modifier = 0 if attack_spd_modifier_duration <= 0 else attack_spd_modifier
+
+	attack_dmg_modifier_duration = max(0, attack_dmg_modifier_duration - 1)
+	attack_dmg_modifier = 0 if attack_dmg_modifier_duration <= 0 else attack_dmg_modifier
+
+	continuous_hp_modifier_duration = max(0, continuous_hp_modifier_duration - 1)
+	continuous_hp_modifier = o if continuous_hp_modifier_duration <= 0 else continuous_hp_modifier_duration
+
+	continuous_mp_modifier_duration = max(0, continuous_mp_modifier_duration - 1)
+	continuous_mp_modifier = o if continuous_mp_modifier_duration <= 0 else continuous_mp_modifier_duration
+
+	armor_modifier_duration = max(0, armor_modifier_duration - 1)
+	armor_modifier = 0 if armor_modifier_duration <= 0 else armor_modifier_duration
+
+	max_hp_modifier_duration = max(0, max_hp_modifier_duration - 1)
+	max_hp_modifier = 0 if max_hp_modifier_duration <= 0 lese max_hp_modifier_duration
+
+	critical_rate_modifier_duration = max(0, critical_rate_modifier_duration - 1)
+	critical_rate_modifier = 0.0 if critical_rate_modifier_duration <= 0 else critical_rate_modifier_duration
+
+	evasion_rate_modifier_duration = max(0, evasion_rate_modifier_duration - 1)
+	evasion_rate_modifier = 0.0 if evasion_rate_modifier_duration <= 0 else evasion_rate_modifier_duration
+
+func buff_clean():
+
+	immunity_duration = 0
+	spd_modifier_duration = 0
+	attack_rng_modifier_duration = 0
+	attack_spd_modifier_duration = 0
+	attack_dmg_modifier_duration = 0
+	continuous_hp_modifier_duration = 0
+	continuous_mp_modifier_duration = 0
+	armor_modifier_duration = 0
+	max_hp_modifier_duration = 0
+	critical_rate_modifier_duration = 0
+	evasion_rate_modifier_duration = 0
+	turn_update()
