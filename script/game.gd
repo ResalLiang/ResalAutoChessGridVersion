d72398b2535d276930d51e54f18092217c15f37d
Resal <60034532+ResalLiang@users.noreply.github.com>
2025-08-14 16:45:13 +0800

    Update game.gd
    add shop system
    full the play system

--- script/game.gd
+++ script/game.gd
@@ -16,6 +16,7 @@ const hero_class = preload("res://script/hero.gd")
 @onready var shop_refresh_button: Button = $shop_refresh_button
 @onready var shop_freeze_button: Button = $shop_freeze_button
 @onready var remain_coins_label: Label = $remain_coins_label
+@onready var shop_handler: Node = $ShopHandler
 
 var hero_data: Dictionary  # Stores hero stats loaded from JSON
 enum Team { TEAM1, TEAM2, TEAM1_FULL, TEAM2_FULL}
@@ -42,13 +43,58 @@ var rand_hero_ratio := 0.8
 var is_shop_frozen := false
 var remain_coins := 999
 
+var current_round := 1
+var won_rounds := 0
+const max_won_rounds := 5
+var lose_rounds := 0
+const max_lose_rounds := 5
+
+var saved_arena_team
+
 # Define rarity weights dictionary
 const RARITY_WEIGHTS = {
-	"Common": 50,
-	"Uncommon": 30,
-	"Rare": 20,
-	"Epic": 10,
-	"Legendary": 5
+	1: {
+		"Common": 50,
+		"Uncommon": 30,
+		"Rare": 0,
+		"Epic": 0,
+		"Legendary": 0
+	},
+	2: {
+		"Common": 50,
+		"Uncommon": 30,
+		"Rare": 20,
+		"Epic": 0,
+		"Legendary": 0
+	},
+	3: {
+		"Common": 50,
+		"Uncommon": 30,
+		"Rare": 20,
+		"Epic": 10,
+		"Legendary": 0
+	},
+	4: {
+		"Common": 50,
+		"Uncommon": 30,
+		"Rare": 20,
+		"Epic": 10,
+		"Legendary": 5
+	},
+	5: {
+		"Common": 50,
+		"Uncommon": 30,
+		"Rare": 20,
+		"Epic": 10,
+		"Legendary": 5
+	},
+	6: {
+		"Common": 50,
+		"Uncommon": 30,
+		"Rare": 20,
+		"Epic": 10,
+		"Legendary": 5
+	}
 }
 
 signal game_finished
@@ -73,104 +119,54 @@ func _ready():
 		push_error("JSON parsing failed for hero_stats.json")
 		return
 	
-	game_finished.connect(_on_game_finished)
+	game_finished.connect(_on_round_finished)
+	
+	game_start_button.pressed.connect(new_round_prepare_end)
+	game_restart_button.pressed.connect(start_new_game)
+	shop_refresh_button.pressed.connect(shop_refresh)
+	shop_freeze_button.pressed.connect(shop_freeze)
+
+
 	
 
-	# 遍历每个格子
-	for x in range(0, tile_size.x / 2):
-		for y in range(0, tile_size.y):
-			if randf() > rand_hero_ratio:
-				# 创建角色实例
-				var character = hero_scene.instantiate()
-
-				# 计算格子中心位置
-				var position = Vector2(
-					x * tile_size.x + tile_size.x / 2,
-					y * tile_size.y + tile_size.y / 2
-				)
-
-				# 设置角色位置
-				character._position = position
-				character.team = 1
-				character.faction = team1_faction
-				character.hero_name = get_random_character(team1_faction)
-				character.current_play_area = character.play_areas.arena
-				team_dict[Team.TEAM1_FULL].append(character)
-				arena_unit_grid.add_unit(Vector2(x, y), character)
-				# 添加到场景
-				arena_unit_grid.add_child(character)
-				
-	for x in range(tile_size.x / 2, tile_size.x):
-		for y in range(0, tile_size.y):
-			if randf() > rand_hero_ratio:
-				# 创建角色实例
-				var character = hero_scene.instantiate()
-				
-				# 计算格子中心位置
-				var position = Vector2(
-					x * tile_size.x + tile_size.x / 2,
-					y * tile_size.y + tile_size.y / 2
-				)
-				
-				# 设置角色位置
-				character._position = position
-				character.team = 2
-				character.faction = team2_faction
-				character.hero_name = get_random_character(team2_faction)
-				character.current_play_area = character.play_areas.arena
-				team_dict[Team.TEAM2_FULL].append(character)
-				arena_unit_grid.add_unit(Vector2(x, y), character)
-				# 添加到场景
-				arena_unit_grid.add_child(character)
-				
 	center_point = Vector2(tile_size.x * grid_count / 2, tile_size.y * grid_count / 2)
 	hero_mover.setup_before_turn_start()
 	
-	game_start_button.pressed.connect(start_new_round)
-	game_restart_button.pressed.connect(restart_game)
-	shop_refresh_button.pressed.connect(shop_refresh)
-	shop_freeze_button.pressed.connect(shop_freeze)
-	
 	add_child(game_start_button)
-	#start_new_round()
+	add_child(game_restart_button)
+	add_child(shop_refresh_button)
+	add_child(shop_freeze_button)
+
+	shop_refresh(shop_data.shop_level)
+	current_round = 0
+	start_new_game()
 
 func _process(delta: float) -> void:
-	remain_coins_label.text = "Remaining Coins    = " + str(remain_coins)
-	
-	
-func get_random_character(faction_name: String) -> String:
-	if not hero_data.has(faction_name):
-		return ""
-	
-	var candidates = []
-	var weights = []
-	
-	# Prepare candidate list and weight list
-	for char_name in hero_data[faction_name]:
-		var rarity = hero_data[faction_name][char_name]["rarity"]
-		if RARITY_WEIGHTS.has(rarity) and hero_data[faction_name][char_name]["spd"] != 0:
-			candidates.append(char_name)
-			weights.append(RARITY_WEIGHTS[rarity])
-	
-	if candidates.size() == 0:
-		return ""
-	
-	# Perform weighted random selection
-	var total_weight = 0
-	for w in weights:
-		total_weight += w
-	
-	var random_value = randi() % total_weight
-	var weight_sum = 0
-	
-	for i in range(candidates.size()):
-		weight_sum += weights[i]
-		if random_value < weight_sum:
-			return candidates[i]
-	
-	return candidates[0] # Default return first one (shouldn't reach here)
+	remain_coins_label.text = "Remaining Coins    = " + str(shop_handler.remain_coins)
+	if shop_handler.remain_coins < shop_handler.shop_buy_price:
+		shop_refresh_button.disabled = true
 
-func start_new_round():
+
+func start_new_game() -> void:
+	clear_play_area(arena)
+	clear_play_area(shop)
+	clear_play_area(bench)
+
+	shop_handler.shop_init()
+	new_round_prepare_start()
+
+func new_round_prepare_start():
+	clear_play_area(arena)
+	load_arena_team()
+	current_round += 1
+
+func new_round_prepare_end():
+	save_arena_team()
+	generate_enemy(current_round * 500)
+	start_new_turn()
+
+func start_new_turn():
+	# if start new turn, it will be fully auto.
 	game_start_button.disabled = true
 	print("Start new round.")
 	var team1_alive_cnt = 0
@@ -186,14 +182,16 @@ func start_new_round():
 			
 	if team1_alive_cnt == 0:
 		game_finished.emit("team2")
+		return
 	elif team2_alive_cnt == 0:
 		game_finished.emit("team1")
+		return
 		
 	current_team = [Team.TEAM1, Team.TEAM2][randi() % 2]
 	
-	start_team_turn(current_team)
+	start_hero_turn(current_team)
 
-func start_team_turn(team: Team):
+func start_hero_turn(team: Team):
 	team_chars = sort_characters(team, SelectionMode.HIGH_HP)
 	var current_hero = team_chars.pop_front()
 	if hero_mover._get_play_area_for_position(current_hero.global_position) == 0:
@@ -219,18 +217,34 @@ func _on_character_action_finished():
 		
 	var opposing_team = Team.TEAM2 if current_team == Team.TEAM1 else Team.TEAM1
 	if team_dict[opposing_team] != []:
-		start_team_turn(opposing_team)
+		start_hero_turn(opposing_team)
 		var backup_team = opposing_team
 		opposing_team = current_team
 		current_team = backup_team
 		#opposing_team, current_team = current_team, opposing_team
 	elif team_dict[opposing_team] == [] and team_dict[current_team] != []:
-		start_team_turn(current_team)
+		start_hero_turn(current_team)
 	else:
-		start_new_round()
+		start_new_turn()
 
-func _on_game_finished(msg):
-	print("Game over, %s won!" % msg)
+func _on_round_finished(msg):
+	if msg == "team1":
+		won_rounds += 1
+		print("Round %d over, you won!" % current_round)
+	elif msg == "team2":
+		lose_rounds += 1
+		print("Round %d over, %s lose..." % current_round)
+
+	print("You have won %d rounds, and lose %d rounds." % [won_rounds, lose_rounds])
+
+	if won_rounds >= max_won_rounds:
+		print("You won the game!")
+		return
+	elif lose_rounds >= max_lose_rounds:
+		print("You lose the game... Try later.")
+		return
+
+	new_round_prepare_start()
 
 func sort_characters(team: Team, mode: SelectionMode) -> Array:
 	var heroes_team = team_dict[team]
@@ -246,18 +260,76 @@ func sort_characters(team: Team, mode: SelectionMode) -> Array:
 			heroes_team.sort_custom(func(a, b): 
 				return a.position.distance_to(center_point) > b.position.distance_to(center_point))
 	return heroes_team
+	
+func generate_enemy(difficulty : int) -> void:
+	Team.TEAM2_FULL = []
+	for node in get_tree().get_nodes_in_group("hero_group"):
+		if node is Hero and node.current_play_area == play_areas.arena and node.team != 1:
+			node.queue_free()
+	var current_difficulty := 0
+	var current_enemy_cnt := 0
 
-func restart_game() ->  void:
-	pass
+	while current_difficulty < difficulty and current_enemy_cnt <= arena.unit_grid.size.x * arena.unit_grid.size.y / 2:
+		var rand_x = randi_range(arena.unit_grid.size.x / 2, arena.unit_grid.size.x - 1)
+		var rand_y = randi_range(0, arena.unit_grid.size.y - 1)
+		if not arena.unit_grid.is_tile_occupied(Vector2(x, y)):
+			var character = hero_scene.instantiate()
+			character.team = 2
+			character.faction = hero_data.keys()[randi_range(0, hero_data.keys().size() - 1)]
+			character.hero_name = get_random_character(team2_faction)
+			hero_mover._move_hero(character, arena, Vector2(x, y))
+			current_difficulty += character.max_hp
+			current_enemy_cnt += 1
+			Team.TEAM2_FULL.append(character)
 	
-func shop_refresh() -> void:
-	remain_coins -= 3
-	pass
+func get_random_character(faction_name: String) -> String:
+	if not hero_data.has(faction_name):
+		return ""
 	
-func shop_freeze() -> void:
-	if is_shop_frozen:
-		shop_refresh_button.disabled = false
-	else:
-		shop_refresh_button.disabled = true
-	is_shop_frozen = not is_shop_frozen 
-	return
+	var candidates = []
+	var weights = []
+	
+	# Prepare candidate list and weight list
+	for char_name in hero_data[faction_name]:
+		var rarity = hero_data[faction_name][char_name]["rarity"]
+		if RARITY_WEIGHTS[shop_handler.shop_level].has(rarity) and hero_data[faction_name][char_name]["spd"] != 0:
+			candidates.append(char_name)
+			weights.append(RARITY_WEIGHTS[shop_handler.shop_level][rarity])
+	
+	if candidates.size() == 0:
+		return ""
+	
+	# Perform weighted random selection
+	var total_weight = 0
+	for w in weights:
+		total_weight += w
+	
+	var random_value = randi() % total_weight
+	var weight_sum = 0
+	
+	for i in range(candidates.size()):
+		weight_sum += weights[i]
+		if random_value < weight_sum:
+			return candidates[i]
+	
+	return candidates[0] # Default return first one (shouldn't reach here)
+
+func clear_play_area(play_area_to_clear: PlayArea):
+	for node in play_area_to_clear.unit_grid.get_children():
+		if node is Hero:
+			node.queue_free()
+
+func load_arena_team():
+	Team.TEAM1_FULL = []
+	for tile_index in save_arena_team.keys():
+		if saved_arena_team[tile_index]:
+			var character = hero_scene.instantiate()
+			character.team = 1
+			character.faction = saved_arena_team[tile_index].factions
+			character.hero_name = saved_arena_team[tile_index].hero_name
+			hero_mover._move_hero(character, arena, tile_index) 
+			Team.TEAM1_FULL.append(character)
+
+
+func save_arena_team():
+	saved_arena_team = arena.unit_grid.units.duplicate()
