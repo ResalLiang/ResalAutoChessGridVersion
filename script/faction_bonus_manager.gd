77d98229f113b5fbc06e2fd3baf407512f77e19d
Resal <60034532+ResalLiang@users.noreply.github.com>
2025-08-29 16:22:00 +0800

    Update faction_bonus_manager.gd

--- script/faction_bonus_manager.gd
+++ script/faction_bonus_manager.gd
@@ -1,51 +1,23 @@
 class_name FactionBonusManager
 extends Node2D
 
-var elf_bonus := false
-var human_bonus := false
-var dwarf_bonus := false
-var holy_bonus := false
-var forestProtector_bonus := false
-var undead_bonus := false
-var demon_bonus := false
-
-var faction_list = ["elf", "human", "dwarf", "holy", "forestProtector", "undead", "demon"]
-
 @onready var arena: PlayArea = %arena
+@onready var bench: PlayArea = %bench
 
+# bonus_level_list for storing each level need chess count
 var bonus_level_list : Dictionary = {
 	"elf" : [2, 4, 6],
 	"human" : [2, 4, 6],
 	"dwarf" : [2, 4, 6],
-	"holy" : [2, 4, 6],
+	"holy" : [3, 6],
 	"forestProtector" : [2, 4, 6],
 	"undead" : [2, 4, 6],
 	"demon" : [2, 4, 6]	
 }
 
-var bonus_level_dict : Dictionary = {
-	"player" : {
-		"elf" : 0,
-		"human" : 0,
-		"dwarf" : 0,
-		"holy" : 0,
-		"forestProtector" : 0,
-		"undead" : 0,
-		"demon" : 0
-	},
-	"ai" : {
-		"elf" : 0,
-		"human" : 0,
-		"dwarf" : 0,
-		"holy" : 0,
-		"forestProtector" : 0,
-		"undead" : 0,
-		"demon" : 0
-	}
-}
-
-var faction_count : Dictionary = {
-	"player" : {
+# player_faction_count for storing players chess name
+var player_faction_count : Dictionary = {
+	1 : {
 		"elf" : [],
 		"human" : [],
 		"dwarf" : [],
@@ -54,7 +26,7 @@ var faction_count : Dictionary = {
 		"undead" : [],
 		"demon" : []
 	},
-	"ai" : {
+	2 : {
 		"elf" : [],
 		"human" : [],
 		"dwarf" : [],
@@ -65,39 +37,178 @@ var faction_count : Dictionary = {
 	}
 }
 
+# player_bonus_level_dict for summary player bonus level
+var player_bonus_level_dict : Dictionary = {
+	1 : {
+		"elf" : 0,
+		"human" : 0,
+		"dwarf" : 0,
+		"holy" : 0,
+		"forestProtector" : 0,
+		"undead" : 0,
+		"demon" : 0
+	},
+	2 : {
+		"elf" : 0,
+		"human" : 0,
+		"dwarf" : 0,
+		"holy" : 0,
+		"forestProtector" : 0,
+		"undead" : 0,
+		"demon" : 0
+	}
+}
 
-func bonus_refresh():
-	for hero_index in arena.unit_grid.values():
 
-		if not (is_instance_valid(hero_index) and hero_index is Hero):
+func bonus_refresh() -> void:
+	for hero_index in arena.unit_grid.units.values():
+
+		if not is_instance_valid(hero_index) or not hero_index is Hero:
 			continue
 
-		if hero_index.team == 1:
-			if not faction_count["player"][hero_index.faction].has(hero_index.hero_name):
-				faction_count["player"][hero_index.faction].append(hero_index.hero_name)
-		elif hero_index.team == 2:
-			if not faction_count["ai"][hero_index.faction].has(hero_index.hero_name):
-				faction_count["ai"][hero_index.faction].append(hero_index.hero_name)
+		if not player_faction_count[hero_index.team][hero_index.faction].has(hero_index.hero_name):
+			player_faction_count[hero_index.team][hero_index.faction].append(hero_index.hero_name)
 
-	for player_index in ["player", "ai"]:
+	for player_index in player_faction_count.keys():
 		for faction_index in bonus_level_list.keys():
 			var bonus_level = 0
 			for level_value in bonus_level_list[faction_index]:
-				if faction_count[player_index][faction_index].size() >= level_value:
+				if player_faction_count[player_index][faction_index].size() >= level_value:
 					bonus_level += 1
-			bonus_level_dict[player_index][faction_index] = min(bonus_level, 3)
+			player_bonus_level_dict[player_index][faction_index] = min(bonus_level, 3)
 
-func apply_bonus():
-	for player_index in ["player", "ai"]:
-		for faction_index in bonus_level_list.keys():
-			var current_faction_bonus_level = bonus_level_dict[player_index][faction_index]
-			if current_faction_bonus_level > 0:
-				apply_faction_bonus(1, faction_index, current_faction_bonus_level)
+	for hero_index in arena.unit_grid.units.values():
+		if not is_instance_valid(hero_index) or not hero_index is Hero:
+			continue
+
+		var current_bonus_level
+		for player_index in player_faction_count.keys():
+			if hero_index.team == 1 and player_bonus_level_dict[hero_index.team][hero_index.faction] > 0:
+				apply_faction_bonus(hero_index, player_bonus_level_dict[hero_index.team][hero_index.faction])
+
+func apply_faction_bonus(faction: String, bonus_level: int, applier_team: 1) -> void:
+
+	for hero_index in arena.unit_grid.units.values():
+		var friendly_faction_hero : Array[Hero]
+		var friendly_hero : Array[Hero]
+		var enemy_hero : Array[Hero]
+
+		if not is_instance_valid(hero_index) or not hero_index is Hero:
+			continue
+
+		if hero_index.team != applier_team:
+			enemy_hero.append(hero_index)
+		elif hero_index.team == applier_team and hero_index.faction = faction:
+			friendly_faction_hero.append(hero_index)
+		elif hero_index.team == applier_team:
+			friendly_hero.append(hero_index)
 
 
-func apply_faction_bonus(team: int, faction: String, level: int):
-	match faction:
+	match hero.faction:
+
 		"elf":
-			pass
+			if friendly_faction_hero.size() <= 0:
+				return
+
+			for hero_index in friendly_faction_hero:
+				var effect_instance = ChessEffect.new()
+				effect_instance.evasion_rate_modifier = 0.1 * bonus_level
+				effect_instance.evasion_rate_modifier_duration = 999
+				effect_instance.critical_rate_modifier = 0.1 * bonus_level
+				effect_instance.critical_rate_modifier_duration = 999
+				effect_instance.effect_name = "Swift"
+				effect_instance.effect_type = "Buff"
+				effect_instance.effect_applier = "Elf Faction Bonus"
+				hero_index.effect_handler.effect_list.add_to_effect_array(effect_instance)
+				hero_index.effect_handler.add_child(effect_instance)
+
 		"human":
-			pass
+			if friendly_hero.size() <= 0:
+				return
+
+			for hero_index in friendly_hero:
+				var effect_instance = ChessEffect.new()
+				effect_instance.continuous_mp_modifier = 20 * bonus_level
+				effect_instance.continuous_mp_modifier_duration = 999
+				effect_instance.effect_name = "Wisdom"
+				effect_instance.effect_type = "Buff"
+				effect_instance.effect_applier = "Human Faction Bonus"
+				hero_index.effect_handler.effect_list.add_to_effect_array(effect_instance)
+				hero_index.effect_handler.add_child(effect_instance)
+
+		"dwarf":
+			if friendly_hero.size() <= 0:
+				return
+
+			for hero_index in friendly_hero:
+				var effect_instance = ChessEffect.new()
+				effect_instance.armor_modifier = 5 * bonus_level
+				effect_instance.armor_modifier_duration = 999
+				effect_instance.effect_name = "Fortress"
+				effect_instance.effect_type = "Buff"
+				effect_instance.effect_applier = "Dwarf Faction Bonus"
+				hero_index.effect_handler.effect_list.add_to_effect_array(effect_instance)
+				hero_index.effect_handler.add_child(effect_instance)
+
+		"holy":
+			if friendly_faction_hero.size() <= 0:
+				return
+
+			for hero_index in friendly_faction_hero:
+				var effect_instance = ChessEffect.new()
+				effect_instance.immunity_duration = bonus_level
+				effect_instance.effect_name = "Holy Shield"
+				effect_instance.effect_type = "Buff"
+				effect_instance.effect_applier = "Holy Warrior Faction Bonus"
+				hero_index.effect_handler.effect_list.add_to_effect_array(effect_instance)
+				hero_index.effect_handler.add_child(effect_instance)
+
+
+		"forestProtector":
+			if friendly_hero.size() <= 0:
+				return
+
+			for hero_index in friendly_hero:
+				var effect_instance = ChessEffect.new()
+				effect_instance.continuous_hp_modifier = 20 * bonus_level
+				effect_instance.continuous_hp_modifier_duration = 999
+				effect_instance.max_hp_modifier = 30 * bonus_level
+				effect_instance.max_hp_modifier_duration = 999
+				effect_instance.effect_name = "Strong"
+				effect_instance.effect_type = "Buff"
+				effect_instance.effect_applier = "Forest Protector Faction Bonus"
+				hero_index.effect_handler.effect_list.add_to_effect_array(effect_instance)
+				hero_index.effect_handler.add_child(effect_instance)
+
+		"demon":
+			match level:
+			if enemy_hero.size() <= 0:
+				return
+
+			for hero_index in enemy_hero:
+				var effect_instance = ChessEffect.new()
+				effect_instance.continuous_hp_modifier = -5 * bonus_level
+				effect_instance.continuous_hp_modifier_duration = 3
+				effect_instance.silence_duration = bonus_level
+				effect_instance.effect_name = "Doom"
+				effect_instance.effect_type = "Debuff"
+				effect_instance.effect_applier = "Demon Faction Bonus"
+				hero_index.effect_handler.effect_list.add_to_effect_array(effect_instance)
+				hero_index.effect_handler.add_child(effect_instance)
+
+		"undead":
+			match level:
+			if enemy_hero.size() <= 0:
+				return
+
+			for hero_index in enemy_hero:
+				var effect_instance = ChessEffect.new()
+				effect_instance.armor_modifier = -5 * bonus_level
+				effect_instance.armor_modifier_duration = 999
+				effect_instance.spd_modifier = -2
+				effect_instance.spd_modifier_modifier_duration = 999
+				effect_instance.effect_name = "Weak"
+				effect_instance.effect_type = "Buff"
+				effect_instance.effect_applier = "Elf Faction Bonus"
+				hero_index.effect_handler.effect_list.add_to_effect_array(effect_instance)
+				hero_index.effect_handler.add_child(effect_instance)
