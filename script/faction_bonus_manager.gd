ab0bb2350dd8fe65914578965b9e2e014cce7943
Resal <60034532+ResalLiang@users.noreply.github.com>
2025-09-01 15:39:40 +0800

    Update faction_bonus_manager.gd

--- script/faction_bonus_manager.gd
+++ script/faction_bonus_manager.gd
@@ -61,15 +61,15 @@ var player_bonus_level_dict : Dictionary = {
 
 
 func bonus_refresh() -> void:
-	for hero_index in arena.unit_grid.units.values():
+	for chess_index in arena.unit_grid.units.values():
 
-		if not is_instance_valid(hero_index) or not hero_index is Hero:
+		if not is_instance_valid(chess_index) or not chess_index is Chess:
 			continue
 
-		clean_hero_faction_bonus(hero_index)
+		clean_chess_faction_bonus(chess_index)
 
-		if not player_faction_count[hero_index.team][hero_index.faction].has(hero_index.hero_name):
-			player_faction_count[hero_index.team][hero_index.faction].append(hero_index.hero_name)
+		if not player_faction_count[chess_index.team][chess_index.faction].has(chess_index.chess_name):
+			player_faction_count[chess_index.team][chess_index.faction].append(chess_index.chess_name)
 
 	for player_index in player_faction_count.keys():
 		for faction_index in bonus_level_list.keys():
@@ -87,30 +87,30 @@ func bonus_refresh() -> void:
 
 func apply_faction_bonus(faction: String, bonus_level: int, applier_team: int) -> void:
 
-	var friendly_faction_hero : Array[Hero]
-	var friendly_hero : Array[Hero]
-	var enemy_hero : Array[Hero]
+	var friendly_faction_chess : Array[Chess]
+	var friendly_chess : Array[Chess]
+	var enemy_chess : Array[Chess]
 	
-	for hero_index in arena.unit_grid.units.values():
+	for chess_index in arena.unit_grid.units.values():
 
-		if not is_instance_valid(hero_index) or not hero_index is Hero:
+		if not is_instance_valid(chess_index) or not chess_index is Chess:
 			continue
 
-		if hero_index.team != applier_team:
-			enemy_hero.append(hero_index)
-		elif hero_index.team == applier_team and hero_index.faction == faction:
-			friendly_faction_hero.append(hero_index)
-		elif hero_index.team == applier_team:
-			friendly_hero.append(hero_index)
+		if chess_index.team != applier_team:
+			enemy_chess.append(chess_index)
+		elif chess_index.team == applier_team and chess_index.faction == faction:
+			friendly_faction_chess.append(chess_index)
+		elif chess_index.team == applier_team:
+			friendly_chess.append(chess_index)
 
 
 	match faction:
 
 		"elf":
-			if friendly_faction_hero.size() <= 0:
+			if friendly_faction_chess.size() <= 0:
 				return
 
-			for hero_index in friendly_faction_hero:
+			for chess_index in friendly_faction_chess:
 				var effect_instance = ChessEffect.new()
 				effect_instance.evasion_rate_modifier = 0.1 * bonus_level
 				effect_instance.evasion_rate_modifier_duration = 999
@@ -119,56 +119,56 @@ func apply_faction_bonus(faction: String, bonus_level: int, applier_team: int) -
 				effect_instance.effect_name = "Swift"
 				effect_instance.effect_type = "Faction Bonus"
 				effect_instance.effect_applier = "Elf Faction Bonus"
-				hero_index.effect_handler.add_to_effect_array(effect_instance)
-				hero_index.effect_handler.add_child(effect_instance)
+				chess_index.effect_handler.add_to_effect_array(effect_instance)
+				chess_index.effect_handler.add_child(effect_instance)
 
 		"human":
-			if friendly_hero.size() <= 0:
+			if friendly_chess.size() <= 0:
 				return
 
-			for hero_index in friendly_hero:
+			for chess_index in friendly_chess:
 				var effect_instance = ChessEffect.new()
 				effect_instance.continuous_mp_modifier = 20 * bonus_level
 				effect_instance.continuous_mp_modifier_duration = 999
 				effect_instance.effect_name = "Wisdom"
 				effect_instance.effect_type = "Faction Bonus"
 				effect_instance.effect_applier = "Human Faction Bonus"
-				hero_index.effect_handler.add_to_effect_array(effect_instance)
-				hero_index.effect_handler.add_child(effect_instance)
+				chess_index.effect_handler.add_to_effect_array(effect_instance)
+				chess_index.effect_handler.add_child(effect_instance)
 
 		"dwarf":
-			if friendly_hero.size() <= 0:
+			if friendly_chess.size() <= 0:
 				return
 
-			for hero_index in friendly_hero:
+			for chess_index in friendly_chess:
 				var effect_instance = ChessEffect.new()
 				effect_instance.armor_modifier = 5 * bonus_level
 				effect_instance.armor_modifier_duration = 999
 				effect_instance.effect_name = "Fortress"
 				effect_instance.effect_type = "Faction Bonus"
 				effect_instance.effect_applier = "Dwarf Faction Bonus"
-				hero_index.effect_handler.add_to_effect_array(effect_instance)
-				hero_index.effect_handler.add_child(effect_instance)
+				chess_index.effect_handler.add_to_effect_array(effect_instance)
+				chess_index.effect_handler.add_child(effect_instance)
 
 		"holy":
-			if friendly_faction_hero.size() <= 0:
+			if friendly_faction_chess.size() <= 0:
 				return
 
-			for hero_index in friendly_faction_hero:
+			for chess_index in friendly_faction_chess:
 				var effect_instance = ChessEffect.new()
 				effect_instance.immunity_duration = bonus_level
 				effect_instance.effect_name = "Holy Shield"
 				effect_instance.effect_type = "Faction Bonus"
 				effect_instance.effect_applier = "Holy Warrior Faction Bonus"
-				hero_index.effect_handler.add_to_effect_array(effect_instance)
-				hero_index.effect_handler.add_child(effect_instance)
+				chess_index.effect_handler.add_to_effect_array(effect_instance)
+				chess_index.effect_handler.add_child(effect_instance)
 
 
 		"forestProtector":
-			if friendly_hero.size() <= 0:
+			if friendly_chess.size() <= 0:
 				return
 
-			for hero_index in friendly_hero:
+			for chess_index in friendly_chess:
 				var effect_instance = ChessEffect.new()
 				effect_instance.continuous_hp_modifier = 20 * bonus_level
 				effect_instance.continuous_hp_modifier_duration = 999
@@ -177,14 +177,14 @@ func apply_faction_bonus(faction: String, bonus_level: int, applier_team: int) -
 				effect_instance.effect_name = "Strong"
 				effect_instance.effect_type = "Faction Bonus"
 				effect_instance.effect_applier = "Forest Protector Faction Bonus"
-				hero_index.effect_handler.add_to_effect_array(effect_instance)
-				hero_index.effect_handler.add_child(effect_instance)
+				chess_index.effect_handler.add_to_effect_array(effect_instance)
+				chess_index.effect_handler.add_child(effect_instance)
 
 		"demon":
-			if enemy_hero.size() <= 0:
+			if enemy_chess.size() <= 0:
 				return
 
-			for hero_index in enemy_hero:
+			for chess_index in enemy_chess:
 				var effect_instance = ChessEffect.new()
 				effect_instance.continuous_hp_modifier = -5 * bonus_level
 				effect_instance.continuous_hp_modifier_duration = bonus_level
@@ -192,14 +192,14 @@ func apply_faction_bonus(faction: String, bonus_level: int, applier_team: int) -
 				effect_instance.effect_name = "Doom"
 				effect_instance.effect_type = "Faction Bonus"
 				effect_instance.effect_applier = "Demon Faction Bonus"
-				hero_index.effect_handler.add_to_effect_array(effect_instance)
-				hero_index.effect_handler.add_child(effect_instance)
+				chess_index.effect_handler.add_to_effect_array(effect_instance)
+				chess_index.effect_handler.add_child(effect_instance)
 
 		"undead":
-			if enemy_hero.size() <= 0:
+			if enemy_chess.size() <= 0:
 				return
 
-			for hero_index in enemy_hero:
+			for chess_index in enemy_chess:
 				var effect_instance = ChessEffect.new()
 				effect_instance.armor_modifier = -5 * bonus_level
 				effect_instance.armor_modifier_duration = 999
@@ -208,15 +208,15 @@ func apply_faction_bonus(faction: String, bonus_level: int, applier_team: int) -
 				effect_instance.effect_name = "Weak"
 				effect_instance.effect_type = "Faction Bonus"
 				effect_instance.effect_applier = "Elf Faction Bonus"
-				hero_index.effect_handler.add_to_effect_array(effect_instance)
-				hero_index.effect_handler.add_child(effect_instance)
+				chess_index.effect_handler.add_to_effect_array(effect_instance)
+				chess_index.effect_handler.add_child(effect_instance)
 
-func clean_hero_faction_bonus(hero: Hero) -> void:
-	var hero_effect_list = hero.effect_handler.effect_list.duplicate()
-	if hero_effect_list.size() == 0:
+func clean_chess_faction_bonus(chess: Chess) -> void:
+	var chess_effect_list = chess.effect_handler.effect_list.duplicate()
+	if chess_effect_list.size() == 0:
 		return
 
-	hero.effect_handler.effect_list = []
-	for effect_index in hero_effect_list:
+	chess.effect_handler.effect_list = []
+	for effect_index in chess_effect_list:
 		if not effect_index.effect_applier == "Faction Bonus":
-			hero.effect_handler.add_to_effect_array(effect_index)
+			chess.effect_handler.add_to_effect_array(effect_index)
