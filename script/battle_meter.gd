64044fb1a9113612da08f5fa57fd4dce5549b3e0
Resal <60034532+ResalLiang@users.noreply.github.com>
2025-09-01 15:18:13 +0800

    Update battle_meter.gd

--- script/battle_meter.gd
+++ script/battle_meter.gd
@@ -9,9 +9,9 @@ var battle_data: Dictionary = {}
 var battle_meter_type := DAMAGE_TYPE.DAMAGE_APPLIED
 
 
-func get_damage_data(hero: Hero, value: int, attacker: Hero):
-	var attacker_index = [attacker.faction, attacker.hero_name, attacker.hero_serial, attacker.team]
-	var hero_index = [hero.faction, hero.hero_name, hero.hero_serial, hero.team]
+func get_damage_data(obstacle: Obstacle, value: int, attacker: Obstacle):
+	var attacker_index = [attacker.faction, attacker.chess_name, attacker.chess_serial, attacker.team]
+	var chess_index = [obstacle.faction, obstacle.chess_name, obstacle.chess_serial, obstacle.team]
 
 	if battle_data.has(attacker_index):
 		battle_data[attacker_index][0] += value
@@ -19,17 +19,17 @@ func get_damage_data(hero: Hero, value: int, attacker: Hero):
 		battle_data[attacker_index] = [0, 0, 0, 0] #[damage_applied, damage_taken, heal_applied, heal_taken]
 		battle_data[attacker_index][0] += value
 
-	if battle_data.has(hero_index):
-		battle_data[hero_index][1] += value
+	if battle_data.has(chess_index):
+		battle_data[chess_index][1] += value
 	else:
-		battle_data[hero_index] = [0, 0, 0, 0]
-		battle_data[hero_index][1] += value
+		battle_data[chess_index] = [0, 0, 0, 0]
+		battle_data[chess_index][1] += value
 
 	update_ranking()
 
-func get_heal_data(hero: Hero, value: int, healer: Hero):
-	var healer_index = [healer.faction, healer.hero_name, healer.hero_serial, healer.team]
-	var hero_index = [hero.faction, hero.hero_name, hero.hero_serial, hero.team]
+func get_heal_data(obstacle: Obstacle, value: int, healer: Obstacle):
+	var healer_index = [healer.faction, healer.chess_name, healer.chess_serial, healer.team]
+	var chess_index = [obstacle.faction, obstacle.chess_name, obstacle.chess_serial, obstacle.team]
 
 	if battle_data.has(healer_index):
 		battle_data[healer_index][2] += value
@@ -37,11 +37,11 @@ func get_heal_data(hero: Hero, value: int, healer: Hero):
 		battle_data[healer_index] = [0, 0, 0, 0] #[damage_applied, damage_taken, heal_applied, heal_taken]
 		battle_data[healer_index][2] += value
 
-	if battle_data.has(hero_index):
-		battle_data[hero_index][3] += value
+	if battle_data.has(chess_index):
+		battle_data[chess_index][3] += value
 	else:
-		battle_data[hero_index] = [0, 0, 0, 0]
-		battle_data[hero_index][3] += value
+		battle_data[chess_index] = [0, 0, 0, 0]
+		battle_data[chess_index][3] += value
 
 	update_ranking()
 
@@ -64,8 +64,8 @@ func update_ranking():
 	battle_array.sort_custom(func(a, b): return a[1] > b[1])
 	var battle_array_sliced = battle_array.slice(0, min(10, battle_array.size()))
 	
-	for hero in battle_array_sliced:
-		if hero[1] > 0:
+	for chess in battle_array_sliced:
+		if chess[1] > 0:
 			var item = battle_meter_bar_scene.instantiate()
-			item.init(hero[0][0], hero[0][1], hero[0][3], battle_array_sliced[0][1], hero[1])
+			item.init(chess[0][0], chess[0][1], chess[0][3], battle_array_sliced[0][1], chess[1])
 			add_child(item)
