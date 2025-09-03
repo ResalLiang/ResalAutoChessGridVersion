7a9dae7dea1edae8cd9611cea0e84f1deddf0d55
Resal <60034532+ResalLiang@users.noreply.github.com>
2025-09-03 17:10:21 +0800

    Update main.gd

--- script/main.gd
+++ script/main.gd
@@ -1,36 +1,33 @@
 extends Node2D
 class_name Main
 
-@onready var menu_container: Control = $menu_container
-@onready var game_container: Control = $game_container
+@onready var main_container: Control = $main_container
 @onready var transition_layer: ColorRect = $transition_layer
 
 # 当前活跃的场景
 var current_scene: Node2D = null
-var current_scene_name : String
 
 func _ready():
 	# 初始化显示主菜单
+	main_container.mouse_filter = MouseFilter.MOUSE_FILTER_PASS
 	show_main_menu()
 
 # 显示主菜单
 func show_main_menu():
-	_transition_to_scene("res://scene/menu.tscn", menu_container)
-	current_scene_name = "menu"
-	#current_scene.game_started.connect(show_game)
+	_transition_to_scene("res://scene/menu.tscn", main_container)
+
 # 显示游戏场景
 func show_game():
-	_transition_to_scene("res://scene/game.tscn", game_container)
-	current_scene_name = "game"
+	_transition_to_scene("res://scene/game.tscn", main_container)
 
 # 显示设置菜单
 func show_settings():
-	_transition_to_scene("res://scene/menus/SettingsMenu.tscn", menu_container)
-	current_scene_name = "setting"
+	_transition_to_scene("res://scene/setting.tscn", main_container)
 
 # 场景切换核心方法 - 这是自定义方法
 func _transition_to_scene(scene_path: String, container: Control):
 	# 显示过渡效果
+	transition_layer.show()
 	var tween = create_tween()
 	tween.tween_property(transition_layer, "color", Color(0, 0, 0, 1), 0.5)
 	await tween.finished
