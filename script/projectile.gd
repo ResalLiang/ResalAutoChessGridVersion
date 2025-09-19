46ad46611d9a3000d98403321969ceea14709e92
Resal <60034532+ResalLiang@users.noreply.github.com>
2025-09-19 16:27:09 +0800

    Update projectile.gd

--- script/projectile.gd
+++ script/projectile.gd
@@ -4,6 +4,9 @@ extends Area2D
 @export var damage: float = 20.0
 @export var penetration: int = 1  # 穿透次数
 @export var max_distance: float = 300.0  # 最大飞行距离
+@export var decline_ratio := 3.0
+
+var current_penetration
 
 var direction: Vector2 = Vector2.ZERO:
 	set(value):
@@ -38,6 +41,8 @@ func _ready():
 	if initial_flip:
 		animated_sprite_2d.flip_h = true
 		animated_sprite_2d.rotation_degrees = 180  # 根据实际美术资源调整
+
+	current_penetration = penetration
 		
 	animated_sprite_2d.play("default")
 
@@ -60,7 +65,7 @@ func _physics_process(delta):
 		traveled_distance += movement.length()
 		
 		# 超出最大距离或穿透次数耗尽时消失
-		if traveled_distance > max_distance or penetration <= 0 or damage < 5:
+		if traveled_distance > max_distance or current_penetration <= 0 or damage < 5:
 			projectile_vanished.emit()
 			queue_free()
 
@@ -71,11 +76,12 @@ func _on_area_entered(area):
 		# 只伤害敌方队伍
 		if obstacle.team != source_team and attacker != null:
 			obstacle.take_damage(damage, attacker)
-			penetration -= 1  # 减少穿透计数
-			damage /= 3.0
+			await projectile_damage_display(obstacle, damage)
+			current_penetration -= 1  # 减少穿透计数
+			damage /= decline_ratio
 
 			# 穿透次数耗尽时消失
-			if penetration <= 0 or damage < 5:
+			if current_penetration <= 0 or damage < 5:
 				projectile_vanished.emit()
 				queue_free()
 
@@ -83,3 +89,48 @@ func _on_area_entered(area):
 func _on_visibility_notifier_screen_exited():
 	projectile_vanished.emit()
 	queue_free()
+
+
+func projectile_damage_display(chess: Obstacle, display_value: float):
+
+	if display_value <= 0:
+		return
+
+	var battle_label = Label.new()
+	battle_label.z_index = 6
+	add_child(battle_label)
+
+	# Create a new theme
+	var new_theme = Theme.new()
+	
+	# Load font resource
+	var font = load("res://asset/font/Everyday_Tiny.ttf") as FontFile
+	
+	# Set font and size in theme using correct methods
+	new_theme.set_font("font", "Label", font)
+	
+	# Apply theme to label
+	battle_label.theme = new_theme
+	
+	var label_settings = LabelSettings.new()
+	label_settings.font_size = 4
+
+	label_settings.font_color = Color.YELLOW
+	battle_label.text = str(display_value)
+
+	battle_label.label_settings = label_settings
+	
+	var old_position = chess.global_position + Vector2(8, -8)
+	battle_label.global_position = old_position
+
+	var damage_tween
+	if damage_tween:
+		damage_tween.kill() # Abort the previous animation.
+	damage_tween = create_tween().set_parallel(true)
+	damage_tween.set_ease(Tween.EASE_IN_OUT)
+	damage_tween.set_trans(Tween.TRANS_CUBIC)
+	damage_tween.tween_property(battle_label, "global_position", old_position + Vector2(0, -16), 1.0)
+	damage_tween.tween_property(battle_label,"modulate.a", 0.0, 1.0)
+	await damage_tween.finished
+	damage_tween.kill()
+	battle_label.queue_free()
