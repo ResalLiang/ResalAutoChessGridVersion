fcca6e4f2713d8dee2861be2c9dbd30e782e43b8
Resal <60034532+ResalLiang@users.noreply.github.com>
2025-09-29 15:21:35 +0800

    Update damage_manager.gd

--- script/damage_manager.gd
+++ script/damage_manager.gd
@@ -10,7 +10,7 @@ func damage_handler(attacker: Obstacle, target: Obstacle, damage_value: float, d
 	if damage_result <= 0:
 		return
 
-	if not ["Melee_attack", "Ranged_attack", "Magic_attack", "Continuous_effect"].has(damage_type):
+	if not ["Melee_attack", "Ranged_attack", "Magic_attack", "Continuous_effect", "Free_strike"].has(damage_type):
 		return
 
 	if not is_instance_valid(target) or target.visible == false or not target is Obstacle or target.status == target.STATUS.DIE:
@@ -20,22 +20,42 @@ func damage_handler(attacker: Obstacle, target: Obstacle, damage_value: float, d
 		return
 
 	if target is Chess:
-		if target.get("evasion_rate") != null and randf() <= target.evasion_rate and not affix_array.has("ignore_evasion"):
+		if (target.get("evasion_rate") != null and randf() <= target.evasion_rate and not affix_array.has("ignore_evasion")) or (damage_type == "Free_strike" and target.effect_handler.is_parry):
 			target.attack_evased.emit(target, attacker)
 			return
+
+	var speller_bonus_level = attacker.faction_bonus_manager.get_bonus_level("speller", attacker.team) if attacker is Chess else 0
 
 	if attacker is Chess:
 		if attacker.get("critical_rate") != null and (randf() <= attacker.critical_rate and damage_type != "Magic_attack" and not target.effect_handler.is_critical_immunity):
 			damage_result *= attacker.critical_damage
 			critical_damage = true
+		elif speller_bonus_level > 0 and attacker is Chess and attacker.role == "speller" and damage_type == "Magic_attack":
+			if rand_f() <= speller_bonus_level * 0.1:
+				damage_result *= attacker.critical_damage
+				critical_damage = true				
+
+			
+	var elf_bonus_level = attacker.faction_bonus_manager.get_bonus_level("elf", attacker.team) if attacker is Chess else 0
+	var pikeman_bonus_level = attacker.faction_bonus_manager.get_bonus_level("pikeman", attacker.team) if attacker is Chess else 0
+
+	var min_damage_value = elf_bonus_level if elf_bonus_level > 0 else 1
+
+	if pikeman_bonus_level > 0 and target is Chess and target.role == "knight" and attacker is Chess and attacker.role == "pikeman":
+		damage_result += (5 * pikeman_bonus_level)
 
 	if not affix_array.has("ignore_armor") and damage_type != "Magic_attack":
 		damage_result -= target.armor
 		if damage_result < 0:
 			return
-			
-	var elf_bonus_level = attacker.faction_bonus_manager.get_bonus_level("elf", attacker.team) if attacker is Chess else 0
-	var min_damage_value = elf_bonus_level if elf_bonus_level > 0 else 1
+
+	if target is Chess and target.is_phantom:
+		damage_result *= 2.5
+
+	if attacker is Chess and attacker.is_phantom:
+		damage_result /= 2.5
+		
+	#====================do not modify damage_result after============================
 
 	damage_result = max(damage_result, min_damage_value)
 
